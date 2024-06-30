e62e8d676d62d42f4513725e4f7a8a06d65cfd40
Thijs Notkamp <thijs@veryconnect.com>
2024-06-30 11:15:35 +0100

    Initial commit

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  public readonly photos: UserPhoto[] = [];
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+  
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath!
+    });
+  }
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
